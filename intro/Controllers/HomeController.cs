a585a476ed10a00d2a95f57a6db4eb5042716202
Michael <admin@TA-826-04>
2018-12-10 14:06:35 +0100

    Intro oprettet

---
+++ intro/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            string navn = "element";
+
+            return Json(navn, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
