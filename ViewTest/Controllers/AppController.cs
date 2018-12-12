c0c6f53039f33af2e1c6f7b1a4e523db9187b2e6
Michael <admin@TA-826-04>
2018-12-12 15:06:36 +0100

    Master

---
+++ ViewTest/Controllers/AppController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewTest.Models;
+
+namespace ViewTest.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        [Route("~/")]
+        [Route("~/App")]
+        public ActionResult Index()
+        {
+            List<String> tmp = new List<string>();
+            for (int i = 0; i < 10; i++)
+                tmp.Add(Guid.NewGuid().ToString());
+
+            return View(tmp);
+        }
+
+        [HttpGet]
+        [Route("~/Test")]
+        public ActionResult Test()
+        {
+            Person p = new Person() { Navn = "aa", Alder = 10, ErILive = true };
+            return View(p);
+        }
+
+    }
+}
---
+++ ViewTest/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewTest.Models
+{
+    public class Person
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+        public bool ErILive { get; set; }
+
+    }
+}
