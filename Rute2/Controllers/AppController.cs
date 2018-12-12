0e3f36d768d5279b56121705a2f25ac9174571df
Michael <admin@TA-826-04>
2018-12-12 12:33:55 +0100

    RouteAttribute

---
+++ Rute2/Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rute2.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        [HttpGet]
+        [Route("~/")]
+        [Route("~/Forside")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/salg/{kundeid:int?}")]
+        public ActionResult Salg(int? kundeid)
+        {
+            ViewBag.kundeid = kundeid;
+            return View();
+        }
+
+    }
+}
